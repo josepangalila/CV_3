777d82062ecf070dbc9aecda7b88d802aa36c47e
jose pangalila <70064606+josepangalila@users.noreply.github.com>
2020-12-10 14:49:50 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const  person = {
+    name: "jose",
+    age: 19,
+    status: "Mahasiswa",
+    addres: "Manado",
+    bekerja: function () {
+        console.log("Costumer Service");
+    },
+    bekerja: function () {
+        console.log("Programing");
+    },
+};
+
+console.log(person);
+person.bekerja();
+
+function helloWord() {
+    let value = document.getElementById("umur").value;
+    console.log(value);
+}
